8d21e3956217a4a0fca356ff001c9693923523fd
Nandhuvenkat <nandhithait3@gmail.com>
2017-06-23 12:27:46 +0530

    Add files via upload

---
+++ DiveApplication.java
@@ -0,0 +1,12 @@
+package com.nandhu.dive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveApplication.class, args);
+	}
+}
